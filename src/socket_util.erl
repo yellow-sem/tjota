3db8e1973475cdcb6fe139a2d05d9c0600c3b87d
Sebastian Nogara <snogaraleal@gmail.com>
2016-10-22 15:07:58 +0200

    Make socket utility processes tail recursive

--- src/socket_util.erl
+++ src/socket_util.erl
@@ -21,12 +21,12 @@ acceptor_start(#s_handler{} = Handler) ->
 acceptor_loop(#s_handler{} = Handler) ->
     case gen_tcp:accept(Handler#s_handler.socket) of
         {ok, Socket} ->
-            Handler#s_handler.owner ! {acceptor, {socket, Socket}},
-            acceptor_loop(Handler);
+            Handler#s_handler.owner ! {acceptor, {socket, Socket}};
 
         {error, Reason} ->
             Handler#s_handler.owner ! {acceptor, {error, Reason}}
-    end.
+    end,
+    acceptor_loop(Handler).
 
 receiver_start(Socket) when is_port(Socket) ->
     receiver_start(#s_handler{socket = Socket, owner = self()});
@@ -37,9 +37,9 @@ receiver_start(#s_handler{} = Handler) ->
 receiver_loop(#s_handler{} = Handler) ->
     case gen_tcp:recv(Handler#s_handler.socket, 0) of
         {ok, Payload} ->
-            Handler#s_handler.owner ! {receiver, {payload, Payload}},
-            receiver_loop(Handler);
+            Handler#s_handler.owner ! {receiver, {payload, Payload}};
 
         {error, Reason} ->
             Handler#s_handler.owner ! {receiver, {error, Reason}}
-    end.
+    end,
+    receiver_loop(Handler).
